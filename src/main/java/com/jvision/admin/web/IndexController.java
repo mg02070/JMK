f3e45b8474b686cb8c61f0493a5e68bdf8ff0717
unknown <mg02070@hub.com>
2021-01-31 12:59:23 +0900

    Initial commit

---
+++ src/main/java/com/jvision/admin/web/IndexController.java
@@ -0,0 +1,46 @@
+package com.jvision.admin.web;
+
+import com.jvision.admin.service.PostsService;
+import com.jvision.admin.web.dto.PostsResponseDto;
+import com.jvision.admin.web.dto.PostsSaveRequestDto;
+import com.jvision.admin.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("posts", dto);
+        return "posts-update";
+    }
+
+    @GetMapping("/posts/deleteAll")
+    public String postsDeleteAll() {
+        return "posts-deleteAll";}
+        {
+
+    }
+
+
+
+
+}
